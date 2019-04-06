aed236a592ab679ad27ee9feb79e0163f92d1cbf
claire <clhsu@mit.edu>
2019-04-06 19:43:39 -0400

    google API refs, pulls autocomplete place location info

--- app/assets/javascripts/components/App.jsx
+++ app/assets/javascripts/components/App.jsx
@@ -1,4 +1,16 @@
+/* global google */
+/* eslint-disable no-undef */
+
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.autocompleteInput = React.createRef();
+}
+  // componentDidMount(){
+  //   console.log(window);
+
+  // }
+
     render() {
       return <div className="container">
           <LandmarksComponent />
--- app/assets/javascripts/components/LandmarksComponent.jsx
+++ app/assets/javascripts/components/LandmarksComponent.jsx
@@ -1,11 +1,21 @@
+/* global google */
+
 class LandmarksComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
     render() {
       return <div className="panel">
+      <DestinationComponent />
         <div className="search-container">
           <input type="text" placeholder="Search.." name="search"></input>
           <button type="submit">Search</button>
         </div>
         <h1>Hello, Landmarks</h1>
-      </div>;
+      </div>
     }
   }
+
+  // LandmarksComponent.propTypes = {
+  //     google: any
+  // };
