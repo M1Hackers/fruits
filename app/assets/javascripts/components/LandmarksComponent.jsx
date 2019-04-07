23e3cb8ef2d4044fb1dc809fdbe9076e5d80c521
claire <clhsu@mit.edu>
2019-04-06 21:31:25 -0400

    Passed lat/long back to LandmarkComponent

--- app/assets/javascripts/components/LandmarksComponent.jsx
+++ app/assets/javascripts/components/LandmarksComponent.jsx
@@ -4,17 +4,22 @@ class LandmarksComponent extends React.Component {
     constructor(props) {
       super(props);
       this.places = [ {name: 'Place1', details: 'something'} ];
+      this.state = { lat: 0, long: 0 };
+    }
+    
+    setLatLong(newLat, newLong) {
+      this.setState({ lat: newLat, long: newLong });
     }
 
     render() {
       return <div className="panel">
-      <DestinationComponent />
+      <DestinationComponent setLatLong={this.setLatLong} />
         <div className="search-container">
           <input type="text" placeholder="Search.." name="search"></input>
           <button type="submit">Search</button>
         </div>
         <h1>Hello, Landmarks</h1>
-        <LandmarksComponentTable places={this.places} />
+        <LandmarksComponentTable places={this.places} lat={this.state.lat} long={this.state.long} />
       </div>;
     }
   }
